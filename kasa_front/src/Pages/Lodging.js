2f10139ae5b4ec8bdb17c6c9a4866b4c8f12f97e
Stoyann <stoyann.openclassrooms@gmail.com>
2022-05-12 19:37:03 +1100

    rating ok

--- kasa_front/src/Pages/Lodging.js
+++ kasa_front/src/Pages/Lodging.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "../styles/pages/Lodging/lodging.css";
-import { useLocation } from "react-router-dom";
-import star from "../Assets/icones/red-star.svg";
+import { useLocation, useState } from "react-router-dom";
+import redStar from "../Assets/icones/red-star-icone.png";
+import star from "../Assets/icones/stars-icone.png";
 import Slider from "../components/Slider";
 import LodgingHeader from "../components/LodgingHeader";
 import AccordionList from "../components/AccordionList";
 import AccordionAbout from "../components/AccordionAbout";
 export default function Lodging() {
   const location = useLocation();
-  console.log(location.state.rating);
+
   return (
-    <div className="main-lodging">
+    <main className="main-lodging">
       {" "}
       <Slider />
       <LodgingHeader />
@@ -24,13 +25,28 @@ export default function Lodging() {
         })}
       </ul>
       <div className="hote_container">
-        <div className="hote-rate">
-          <img id="stars" src={star} alt="" />
-          <img src={star} alt="" />
-          <img src={star} alt="" />
-          <img src={star} alt="" />
-          <img src={star} alt="" />
-        </div>
+        <ul className="hote-rate">
+          <li>
+            {" "}
+            <img src={location.state.rating >= 1 ? redStar : star} alt="" />
+          </li>
+          <li>
+            {" "}
+            <img src={location.state.rating >= 2 ? redStar : star} alt="" />
+          </li>
+          <li>
+            {" "}
+            <img src={location.state.rating >= 3 ? redStar : star} alt="" />
+          </li>
+          <li>
+            {" "}
+            <img src={location.state.rating >= 4 ? redStar : star} alt="" />
+          </li>
+          <li>
+            {" "}
+            <img src={location.state.rating >= 5 ? redStar : star} alt="" />
+          </li>
+        </ul>
         <div className="hote-info">
           <p>{location.state.host.name}</p>
           <img src={location.state.host.picture} alt="" />
@@ -43,6 +59,6 @@ export default function Lodging() {
         />
         <AccordionList />
       </div>
-    </div>
+    </main>
   );
 }
